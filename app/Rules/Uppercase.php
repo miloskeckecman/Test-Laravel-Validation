d96f643b3fe823325f9ee5a8d5131d89f6188c45
milos <milos@nteams.com>
2023-02-15 15:03:50 +0100

    missing files for requests

---
+++ app/Rules/Uppercase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Uppercase implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        if ($value !== ucfirst($value)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The title does not start with an uppercased letter';
+    }
+}
